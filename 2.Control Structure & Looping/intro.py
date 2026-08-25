4ee3c0ba45ad2ef3c92054425328152325c6250a
zafar-03 <zfnanshabhai@gmail.com>
2026-08-25 18:44:44 +0530

    Added

--- 2.Control Structure & Looping/intro.py
+++ 2.Control Structure & Looping/intro.py
@@ -68,20 +68,22 @@ else:
 
 # ========================================
 """
-1. Write a C program to accept two integers and check whether they are equal.
-
-2.Write a C program to check whether a given number is positive or negative.
+1. Write a C program to accept two integers and check whether they are equal. (6)(3)
 
 
-3.Write a C program to check whether a given number is even and divisable by 3.
+2.Write a C program to check whether a given number is positive or negative.(4)
+
+
+3.Write a C program to check whether a given number is even and divisable by 3.(5)
 
 4.Write a C program to read the age of a candidate and determine whether he is eligible to cast his/her own vote.
-
+(1)
 
 5.Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than or Equal to 0,-1 when m is less than 0.
 Test Data : -5
 Expected Output :
 The value of n = -1
+(2)
 
 """
 
