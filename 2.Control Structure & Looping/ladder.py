57b48a7ee2526c590bdde389af1a5160052c0e20
zafar-03 <zfnanshabhai@gmail.com>
2026-08-25 18:34:59 +0530

    Ladder if-else

---
+++ 2.Control Structure & Looping/ladder.py
@@ -0,0 +1,77 @@
+# ladder if-else >> elif statement (block)
+
+# WAP to Grade System.
+# 90>= A
+# 80>= B
+# 65>= C
+# 35>= D
+# F
+
+# student_marks = int(input("Enter Marks : "))
+
+# if (student_marks>=90 and student_marks<=100):
+#     print("A Grade")
+# elif (student_marks>=80 and student_marks<90):
+#     print("B Grade")
+# elif (student_marks >=65 and student_marks<80):
+#     print("C Grade")
+# elif (student_marks >=35 and student_marks<65):
+#     print("D Grade")
+# elif (student_marks >=0 and student_marks<35):
+#     print("F Grade")
+# else:
+#     print("InValid Marks")
+
+
+
+# WAP to Check which number is Greater.
+# 3
+
+# num_1 = int(input("Enter N1 :"))
+# num_2 = int(input("Enter N2 :"))
+# num_3 = int(input("Enter N3 :"))
+
+
+# if (num_1>num_2 and num_1>num_3):
+#     print("Num_1 is Greater")
+# elif(num_2>num_1 and num_2>num_3):
+#     print("Num_2 is Greater")
+# elif(num_3>num_2 and num_3>num_1):
+#     print("Num_3 is Greater")
+# elif(num_1==num_2 and num_1>num_3):
+#     print("Num_1 and Num_2 are Equal and Greater")
+# elif(num_1==num_3 and num_1>num_2):
+#     print("Num_1 and Num_3 are Equal and Greater")
+# elif(num_2==num_3 and num_2>num_1):
+#     print("Num_2 and Num_3 are Equal and Greater")
+# else:
+#     print("All are Equal")
+
+"""
+1. Write a C program to accept a coordinate point in an XY coordinate system and determine in which quadrant the coordinate point lies.
+Test Data : 7 9
+Expected Output :
+The coordinate point (7,9) lies in the First quadrant.
+
+
+
+2. Write a C program to read temperature in centigrade and display a suitable message according to the temperature state below:
+Temp < 0 then Freezing weather
+Temp 0-10 then Very Cold weather
+Temp 10-20 then Cold weather
+Temp 20-30 then Normal in Temp
+Temp 30-40 then Its Hot
+Temp >=40 then Its Very Hot
+Test Data :
+42
+Expected Output :
+Its very hot.
+
+
+
+3. Write a C program to check whether a triangle is Equilateral, Isosceles or Scalene.
+Test Data :
+50 50 60
+Expected Output :
+This is an isosceles triangle.
+"""
